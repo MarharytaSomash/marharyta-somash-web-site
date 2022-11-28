d14ab462ab26bf48876cc661ad6d00a631d1d6a7
SomashMargarita <maargaritasivceva@gmail.come>
2022-11-28 22:03:54 +0200

    add error handling

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,22 @@
-import styles from '../styles/404.module.scss'
-const Error = () => (
-   <div className={styles.wrap}>
-      <h1>404</h1>
-      <p > Something is going wrong...</p>
-   </div>
-)
+import styles from '../styles/404.module.scss';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
+const Error = () => {
+   const router = useRouter();
+
+   useEffect(() => {
+      setTimeout(() => {
+         router.push('/');
+      }, 2000);
+   }, [router]);
+
+   return (
+      <div className={styles.wrap}>
+         <h1>404</h1>
+         <p > Something is going wrong...</p>
+      </div>
+   )
+   
+}
 export default Error;
